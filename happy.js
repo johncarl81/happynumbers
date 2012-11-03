a6ee495329e1b1aec82c2aa9fa8fb7df82345c95
John Ericksen <johncarl81@gmail.com>
2012-11-02 23:55:41 -0600

    Initial check in of working happy graph(s)

---
+++ happy.js
@@ -0,0 +1,34 @@
+function happyNumberMap(base, max, callback) {
+
+    var happyMap = [];
+
+    for (var i = 1; i <= max; i++) {
+        happyNumberUpdate(happyMap, base, i, callback);
+    }
+
+    return happyMap;
+}
+
+function happyNumberUpdate(happyMap, base, i, callback) {
+
+    if (!happyMap[i]) {
+        happyMap[i] = happyNumber(base, i);
+        callback(i, happyMap[i]);
+
+        happyNumberUpdate(happyMap, base, happyMap[i], callback);
+
+    }
+}
+
+function happyNumber(base, number) {
+
+    var numberLength = Math.floor(Math.log(number) / Math.log(base)) + 1;
+    var happyness = 0;
+
+    for (var i = 0; i < numberLength; i++) {
+        happyness += Math.pow(Math.floor(number / Math.pow(base, i)) % base, 2);
+    }
+
+    return happyness;
+}
+
